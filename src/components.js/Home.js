5309a2e4d131638d5d406f240a9855109a2d0ffb
Arkadiusz Konopka <arekcommerce@gmail.com>
2021-08-27 01:00:17 +0200

    added rest of li elements on homepage

--- src/components.js/Home.js
+++ src/components.js/Home.js
@@ -4,10 +4,13 @@ import { AnimatePresence, motion } from 'framer-motion';
 import colosseum from '../assets/images/HomeImg/colosseum.jpg'
 import fontannaDiTrevi from '../assets/images/HomeImg/fontannaDiTrevi.jpg'
 import forumRomanum from '../assets/images/HomeImg/forumRomanum.jpg'
-import kaplicaSykstyńska from '../assets/images/HomeImg/kaplicaSykstyńska.jpg'
+import kaplicaSykstynska from '../assets/images/HomeImg/kaplicaSykstyńska.jpg'
 import muzeaWatykanskie from '../assets/images/HomeImg/muzeaWatykanskie.jpg'
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import img1 from '../assets/images/backgroundImg/img1.jpg'
 import img2 from '../assets/images/backgroundImg/img2.jpg'
+import img3 from '../assets/images/backgroundImg/img3.jpg'
+import img4 from '../assets/images/backgroundImg/img4.jpg'
 import img5 from '../assets/images/backgroundImg/img5.jpg'
 import RomaView from '../assets/images/backgroundImg/RomaView.jpg'
 
@@ -23,6 +26,18 @@ const Home = () => {
       isHover: false,
       selected: false
     },
+    forumRomanum: {
+      isHover: false,
+      selected: false
+    },
+    muzeaWatykanskie: {
+      isHover: false,
+      selected: false
+    },
+    kaplicaSykstynska: {
+      isHover: false,
+      selected: false
+    },
   })
 
   const onMouseOver = (value) => {
@@ -31,14 +46,50 @@ const Home = () => {
         setMouseOn({
           ...mouseOn,
           colloseum: { ...mouseOn.colloseum, isHover: true, selected: true },
-          fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, selected: false }
+          fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, selected: false },
+          forumRomanum: { ...mouseOn.forumRomanum, selected: false },
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, selected: false },
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, selected: false }
         })
         break;
       case 'fontannaDiTrevi':
         setMouseOn({
           ...mouseOn,
           fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, isHover: true, selected: true },
-          colloseum: { ...mouseOn.colloseum, selected: false }
+          colloseum: { ...mouseOn.colloseum, selected: false },
+          forumRomanum: { ...mouseOn.forumRomanum, selected: false },
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, selected: false },
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, selected: false }
+        })
+        break
+      case 'forumRomanum':
+        setMouseOn({
+          ...mouseOn,
+          fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, selected: false },
+          colloseum: { ...mouseOn.colloseum, selected: false },
+          forumRomanum: { ...mouseOn.forumRomanum, isHover: true, selected: true },
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, selected: false },
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, selected: false }
+        })
+        break
+      case 'muzeaWatykanskie':
+        setMouseOn({
+          ...mouseOn,
+          fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, selected: false },
+          colloseum: { ...mouseOn.colloseum, selected: false },
+          forumRomanum: { ...mouseOn.forumRomanum, selected: false },
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, isHover: true, selected: true },
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, selected: false }
+        })
+        break
+      case 'kaplicaSykstynska':
+        setMouseOn({
+          ...mouseOn,
+          fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, selected: false },
+          colloseum: { ...mouseOn.colloseum, selected: false },
+          forumRomanum: { ...mouseOn.forumRomanum, selected: false },
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, selected: false },
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, isHover: true, selected: true }
         })
         break
     }
@@ -59,6 +110,24 @@ const Home = () => {
           fontannaDiTrevi: { ...mouseOn.fontannaDiTrevi, isHover: false, selected: true },
         })
         break
+      case 'forumRomanum':
+        setMouseOn({
+          ...mouseOn,
+          forumRomanum: { ...mouseOn.forumRomanum, isHover: false, selected: true },
+        })
+        break
+      case 'muzeaWatykanskie':
+        setMouseOn({
+          ...mouseOn,
+          muzeaWatykanskie: { ...mouseOn.muzeaWatykanskie, isHover: false, selected: true },
+        })
+        break
+      case 'kaplicaSykstynska':
+        setMouseOn({
+          ...mouseOn,
+          kaplicaSykstynska: { ...mouseOn.kaplicaSykstynska, isHover: false, selected: true },
+        })
+        break
     }
   }
 
@@ -69,6 +138,12 @@ const Home = () => {
       setBackground(img5)
     } else if (mouseOn.fontannaDiTrevi.isHover) {
       setBackground(img2)
+    } else if (mouseOn.forumRomanum.isHover) {
+      setBackground(img3)
+    } else if (mouseOn.muzeaWatykanskie.isHover) {
+      setBackground(img1)
+    } else if (mouseOn.kaplicaSykstynska.isHover) {
+      setBackground(img4)
     } else {
       setBackground(RomaView)
     }
@@ -102,32 +177,39 @@ const Home = () => {
             </div>
           </div>
 
-          <li className='monument-name m-5 relative'>Forum Romanum
+          <li className='monument-name m-5 relative' onMouseOver={() => onMouseOver('forumRomanum')} onMouseOut={() => onMouseOut('forumRomanum')}>Forum Romanum
             <span className='block w-2/3 h-0.5 bg-gray-200 absolute -bottom-5 -left-10 transition duration'></span></li>
-          <li className='monument-name m-5 relative'>Muzea Watykańskie
+          <div className={`transition duration-500 home-img-container absolute ${mouseOn.forumRomanum.isHover ? 'flex flex-col' : `${mouseOn.forumRomanum.selected ? 'opacity-1' : 'opacity-0'}`}`} >
+            <img src={forumRomanum} alt="fontannaDiTrevi" className='rounded-lg ' />
+            <div className='flex items-center transition duration-1000 delay-300'>
+              <p className='text-xl mr-2 text-gray-300'>Let see something </p><ArrowRightAltIcon fontSize='large' className='text-gray-300' />
+            </div>
+          </div>
+
+
+          <li className='monument-name m-5 relative' onMouseOver={() => onMouseOver('muzeaWatykanskie')} onMouseOut={() => onMouseOut('muzeaWatykanskie')} >Muzea Watykańskie
             <span className='block w-2/3 h-0.5 bg-gray-200 absolute -bottom-5 -left-10 transition duration'></span></li>
-          <li className='monument-name m-5 relative transition'>Kaplica Sykstyńska
+          <div className={`transition duration-500 home-img-container absolute ${mouseOn.muzeaWatykanskie.isHover ? 'flex flex-col' : `${mouseOn.muzeaWatykanskie.selected ? 'opacity-1' : 'opacity-0'}`}`} >
+            <img src={muzeaWatykanskie} alt="fontannaDiTrevi" className='rounded-lg ' />
+            <div className='flex items-center transition duration-1000 delay-300'>
+              <p className='text-xl mr-2 text-gray-300'>Let see something </p><ArrowRightAltIcon fontSize='large' className='text-gray-300' />
+            </div>
+          </div>
+
+          <li className='monument-name m-5 relative' onMouseOver={() => onMouseOver('kaplicaSykstynska')} onMouseOut={() => onMouseOut('kaplicaSykstynska')} >Kaplica Sykstyńska
             <span className='block w-2/3 h-0.5 bg-gray-200 absolute -bottom-5 -left-10 transition duration'></span></li>
+          <div className={`transition duration-500 home-img-container absolute ${mouseOn.kaplicaSykstynska.isHover ? 'flex flex-col' : `${mouseOn.kaplicaSykstynska.selected ? 'opacity-1' : 'opacity-0'}`}`} >
+            <img src={kaplicaSykstynska} alt="fontannaDiTrevi" className='rounded-lg ' />
+            <div className='flex items-center transition duration-1000 delay-300'>
+              <p className='text-xl mr-2 text-gray-300'>Let see something </p><ArrowRightAltIcon fontSize='large' className='text-gray-300' />
+            </div>
+          </div>
+
+
+
         </ul>
       </div>
-      {/* <div className='img-container w-1/2 flex items-center '>
-        <div className='home-img-container ' >
-          <img src={colosseum} alt="colosseum" />
-        </div>
-        <div className='home-img-container hidden'>
-          <img src={fontannaDiTrevi} alt="fontannaDiTrevi" />
-        </div>
-        <div className='home-img-container hidden'>
-          <img src={forumRomanum} alt="forumRomanum" />
-        </div>
-        <div className='home-img-container hidden'>
-          <img src={kaplicaSykstyńska} alt="kaplicaSykstyńska" />
-        </div>
-        <div className='home-img-container hidden'>
-          <img src={muzeaWatykanskie} alt="muzeaWatykanskie" />
-        </div>
 
-    </div> */}
     </div >
   )
 }
